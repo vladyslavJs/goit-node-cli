fafff25ad34a996c3b283d47d12ceacad5113109
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-04-22 22:28:57 +0200

    Update contacts.js

--- src/contacts.js
+++ src/contacts.js
@@ -1,22 +1,68 @@
-// contacts.js
+const fs = require("node:fs/promises");
+const path = require("node:path");
+const crypto = require("node:crypto");
 
-/*
- * Розкоментуй і запиши значення
- * const contactsPath = ;
- */
+const contactsPath = path.resolve("db", "contacts.json");
 
 async function listContacts() {
-  // ...твій код. Повертає масив контактів.
+    const data = await fs.readFile(contactsPath, { encoding: "utf-8" });
+    return JSON.parse(data);
+}
+
+async function writeContacts(contacts) {
+    return fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+}
+
+async function getContacts() {
+    const contacts = await listContacts();
+
+    return contacts;
 }
 
 async function getContactById(contactId) {
-  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+    const contacts = await listContacts();
+
+    const contact = contacts.find((contact) => contact.id === contactId);
+    if (typeof contact === "undefined") {
+        return null;
+    }
+    return contact;
 }
 
 async function removeContact(contactId) {
-  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+    const contacts = await listContacts();
+    const index = contacts.findIndex((contact) => contact.id === contactId);
+
+    if (index === -1) {
+        return null;
+    }
+
+    const removeContact = contacts[index];
+
+    contacts.splice(index, 1);
+    await writeContacts(contacts);
+
+    return removeContact;
 }
 
 async function addContact(name, email, phone) {
-  // ...твій код. Повертає об'єкт доданого контакту (з id).
+    const contacts = await listContacts();
+    const newContact = {
+        id: crypto.randomUUID(),
+        name: name,
+        email: email,
+        phone: phone
+    };
+
+    await writeContacts([...contacts, newContact]);
+    return newContact;
 }
+
+module.exports = {
+    listContacts,
+    writeContacts,
+    getContacts,
+    getContactById,
+    addContact,
+    removeContact
+};
